6cb6126f357b5427e037c555f7b3a936ab1eb66f
discolarrence <discolarrence@gmail.com>
2021-05-28 15:09:26 -0400

    added company 2-4

--- company.py
+++ company.py
@@ -3,7 +3,7 @@ class Employee:
     raise_amt = 1.04  
 
     def __init__(self, first, last, pay):
-        self.t5t4t = first
+        self.first = first
         self.last = last
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
